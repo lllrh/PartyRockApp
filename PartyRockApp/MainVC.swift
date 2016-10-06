bd076d06b1364ca29c1502d14f9293cb2e77001e
Rong Hui Lu <lrh@Rongs-iMac.local>
2016-10-06 02:25:13 -0400

    Finish UI part

---
+++ PartyRockApp/MainVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PartyRockApp
+//
+//  Created by Rong Hui Lu on 2016-10-06.
+//  Copyright © 2016 Rong Hui Lu. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
